0167d168549e3fa0f6a67804f91f9c733f553382
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2022-04-21 00:19:06 +0100

    Now displaying information in the animation panel using the MeshScene object

--- src/EngineGUI/AnimationPanel.cpp
+++ src/EngineGUI/AnimationPanel.cpp
@@ -9,13 +9,16 @@
 #include <FBXFileLoader.h>
 #include <EventManager.h>
 #include <EventNames.h>
+#include <glm/gtx/string_cast.hpp>
+
+#include <MeshScene.h>
 
 using namespace NoxEngine;
 
-void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState *game_state) {
+void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState* game_state) {
 
 	// Variables
-	std::string name = PANEL_NAME_MAP[ PanelName::AnimationSettings ];
+	std::string name = PANEL_NAME_MAP[PanelName::AnimationSettings];
 	bool isAlreadyLoaded = false;
 
 	// Window Begin
@@ -28,6 +31,111 @@ void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState *game_state) {
 		{
 			SIGNAL_EVENT(EventNames::meshAdded, picked_file);
 		}
+	}
+
+	auto startItr = game_state->meshScenes.begin();
+	auto endItr = game_state->meshScenes.end();
+
+	u32 sceneID = 0;
+	while (startItr != endItr)
+	{
+		std::string filename = startItr->first.substr(startItr->first.find_last_of("/\\") + 1);
+		size_t lastIndex = filename.find_last_of(".");
+		std::string base_filename = filename.substr(0, lastIndex);
+
+		// Directly modify the element in the map
+		MeshScene& currentScene = startItr->second;
+
+		if (!currentScene.hasAnimations())
+		{
+			ImGui::Text(base_filename.c_str());
+			startItr++;
+			continue;
+		}
+
+		if (ImGui::TreeNode(base_filename.c_str()))
+		{
+			ImGui::Text("Number of Animations: %i", currentScene.getNumOfAnimations());
+			ImGui::Text("Current Animation: %i", currentScene.animationIndex);
+			ImGui::SameLine();
+			ImGui::PushID(sceneID);
+			if (ImGui::Button("+"))
+			{
+				currentScene.setAnimationIndex(currentScene.animationIndex + 1);
+				currentScene.resetAnimation();
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("-"))
+			{
+				currentScene.setAnimationIndex(currentScene.animationIndex - 1);
+				currentScene.resetAnimation();
+			}
+			ImGui::Text("Animation name: %s", currentScene.animations[currentScene.animationIndex]->mName.C_Str());
+			ImGui::Text("Frame index: %i", currentScene.frameIndex);
+			ImGui::Text("Number of ticks: %i", currentScene.numTicks[currentScene.animationIndex] - 1);
+			ImGui::Text("Duration: %.6f", currentScene.animationDuration[currentScene.animationIndex]);
+
+			f32 duration = currentScene.animationDuration[currentScene.animationIndex];
+
+			if (ImGui::SliderFloat("Duration", &duration, 0.0f, 50.0f))
+				currentScene.resetAnimation();
+
+			currentScene.animationDuration[currentScene.animationIndex] = static_cast<double>(duration);
+
+			ImGui::Text("Progress Bar");
+			ImGui::SameLine();
+
+			// Progress has to be between 0 and 1
+			f32 progress = 0.0f;
+			progress = (float)currentScene.frameIndex / (currentScene.numTicks[currentScene.animationIndex] - 1);
+			ImGui::ProgressBar(progress, ImVec2(0.0f, 0.0f));
+
+			if (ImGui::TreeNode("Nodes Data"))
+			{
+				for (MeshNode2* node : currentScene.allNodes)
+				{
+					ImGui::Text(node->name.c_str());
+					ImGui::Text("Has mesh? %s", node->hasMesh() ? "true" : "false");
+					ImGui::Text("Has animation? %s", node->hasAnimations() ? "true" : "false");
+					if (node->hasAnimations())
+					{
+						if (ImGui::TreeNode("Animation Data"))
+						{
+							glm::mat4 transformaiton = node->getGlobalTransformation(
+								currentScene.frameIndex, currentScene.animationIndex,
+								currentScene.accumulator, currentScene.timeStep,
+								currentScene.whichTickFloor, currentScene.whichTickCeil
+							);
+
+							if (ImGui::BeginTable("Transformations", 4,
+								ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable
+								| ImGuiSelectableFlags_SpanAllColumns))
+							{
+								ImGui::TableNextRow();
+								for (u32 i = 0; i < transformaiton.length(); i++)
+								{
+									ImGui::TableNextRow();
+									for (u32 j = 0; j < transformaiton[i].length(); j++)
+									{
+										ImGui::TableNextColumn();
+										ImGui::Text("%.6f", transformaiton[i][j]);
+									}
+								}
+								ImGui::EndTable();
+							}
+							ImGui::TreePop();
+						}
+					}
+					ImGui::Dummy(ImVec2(0.0f, 20.0f));
+				}
+				ImGui::TreePop();
+			}
+
+			ImGui::PopID();
+			ImGui::TreePop();
+		}
+		sceneID++;
+		startItr++;
 	}
 
 
