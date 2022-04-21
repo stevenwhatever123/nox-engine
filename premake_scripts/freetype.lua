991d47220530b385309faf4d08618cdf4d1e23d8
sharlock93 <sharo@sharlock.dev>
2022-04-22 00:14:12 +0100

    premake update and include harfbuzz for freetype and also change to https

--- premake_scripts/freetype.lua
+++ premake_scripts/freetype.lua
@@ -18,16 +18,32 @@ externalproject "freetype"
 		}
 	}
 
+	include "harfbazz.lua"
+
+	dependson {
+		"harfbazz"
+	}
 
 	if _ACTION ~= 'clean_up' then
-		os.executef('cmake --log-level NOTICE -G %q -B%s -S%s -D %s -D %s -D %s -D %s',
+
+		current_dir = os.getcwd();
+		os.chdir("../libs/build_harfbuzz")
+		os.executef("cmake --build .")
+		os.chdir(current_dir)
+
+		harfbuzz_include = path.getabsolute("../libs/harfbazz/srcinclude")
+		harfbuzz_library = path.getabsolute("../libs/build_harfbuzz/Debug/harfbuzz.lib")
+
+		os.executef('cmake --log-level NOTICE -G %q -B%s -S%s -D %s -D %s -D %s -D %s -D %s -D %s',
 		"Visual Studio 17 2022",
 		"../libs/build_freetype/",
 		"../libs/freetype/",
 		"FT_DISABLE_BROTLI:BOOL=ON",
 		"FT_DISABLE_BZIP2:BOOL=ON",
 		"FT_DISABLE_PNG:BOOL=ON",
-		"FT_DISABLE_ZLIB:BOOL=ON"
+		"FT_DISABLE_ZLIB:BOOL=ON",
+		'HarfBuzz_INCLUDE_DIR:STRING="' .. harfbuzz_include .. '"',
+		'HarfBuzz_LIBRARY:STRING="' .. harfbuzz_library .. '"'
 		)
 	end
 
