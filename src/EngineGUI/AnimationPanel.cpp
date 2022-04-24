8635d901c0b08283157355dc991bc61f0e44cf73
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2022-04-24 02:30:43 +0100

    Update animation sequencer and animation panel. Also some cleaning up

--- src/EngineGUI/AnimationPanel.cpp
+++ src/EngineGUI/AnimationPanel.cpp
@@ -87,6 +87,21 @@ void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState* game_state) {
 			if (ImGui::SliderFloat("Duration", &duration, 0.0f, 50.0f))
 				currentScene.resetAnimation();
 
+			if (!currentScene.playAnimation)
+			{
+				if (ImGui::Button("Play"))
+				{
+					currentScene.playAnimation = !currentScene.playAnimation;
+				}
+			}
+			else
+			{
+				if (ImGui::Button("Stop"))
+				{
+					currentScene.playAnimation = !currentScene.playAnimation;
+				}
+			}
+
 			currentScene.animationDuration[currentScene.animationIndex] = static_cast<double>(duration);
 
 			ImGui::Text("Progress Bar");
@@ -97,115 +112,8 @@ void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState* game_state) {
 			progress = (float)currentScene.frameIndex / (currentScene.numTicks[currentScene.animationIndex] - 1);
 			ImGui::ProgressBar(progress, ImVec2(0.0f, 0.0f));
 
-			if (currentScene.hasAnimations())
-			{
-				// Let try putting the sequencer here
-				if (ImGui::CollapsingHeader("Sequencer"))
-				{
-					AnimationSequencer mySequence(&currentScene);
-					mySequence.mFrameMin = 0;
-					mySequence.mFrameMax = currentScene.numTicks[currentScene.animationIndex] - 1;
-					//mySequence.myItems.push_back(MySequenceItem{0, 10, 30, false});
+			updateSequencer(currentScene);
 
-					// Let's create the sequencer
-					static int selectedEntry = -1;
-					static int firstFrame = 0;
-					static bool expanded = true;
-					static int selectedFrame = 0;
-
-					ImGui::PushItemWidth(130);
-					ImGui::InputInt("Frame Min", &mySequence.mFrameMin);
-					ImGui::SameLine();
-					ImGui::InputInt("Frame ", &selectedFrame);
-					ImGui::SameLine();
-					ImGui::InputInt("Frame Max", &mySequence.mFrameMax);
-					ImGui::PopItemWidth();
-					// Safe guard
-					if (selectedFrame < 0)
-						selectedFrame = 0;
-					if (selectedFrame > currentScene.numTicks[currentScene.animationIndex] - 1)
-						selectedFrame = currentScene.numTicks[currentScene.animationIndex] - 1;
-
-					Sequencer(&mySequence, &selectedFrame, &expanded, &selectedEntry,
-						&firstFrame, ImSequencer::SEQUENCER_EDIT_STARTEND
-						| ImSequencer::SEQUENCER_CHANGE_FRAME);
-
-					//add a UI to edit that particular item
-					if (selectedEntry != -1)
-					{
-						if (currentScene.allNodes[selectedEntry]->hasAnimations())
-						{
-							ImGui::Text("Translation");
-							glm::mat4 translationMatrix = currentScene.allNodes[selectedEntry]
-								->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame];
-							float translate[3] = { translationMatrix[0][3], translationMatrix[1][3], translationMatrix [2][3]};
-							ImGui::DragFloat3("Translation", translate, 0.005f, -100, 100, "%.3f", 0);
-							//Apply value
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[0][3] = translate[0];
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[1][3] = translate[1];
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[2][3] = translate[2];
-
-
-							ImGui::Text("Rotation");
-							float rotation[3] = { currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][0],
-								currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][1] ,
-								currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][2] };
-							ImGui::DragFloat3("Rotation", rotation, 0.005f, -10, 10, "%.3f", 0);
-							//Apply value
-							currentScene.allNodes[selectedEntry]
-								->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][0] = rotation[0];
-							currentScene.allNodes[selectedEntry]
-								->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][1] = rotation[1];
-							currentScene.allNodes[selectedEntry]
-								->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][2] = rotation[2];
-
-
-							ImGui::Text("Scaling");
-							glm::mat4 scalingMatrix = currentScene.allNodes[selectedEntry]
-								->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame];
-							float scaling[3] = { scalingMatrix[0][0], scalingMatrix[1][1], scalingMatrix[2][2]};
-							ImGui::DragFloat3("Scaling", scaling, 0.005f, 1, 100, "%.3f", 0);
-							//Apply value
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[0][0] = scaling[0];
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[1][1] = scaling[1];
-							(currentScene.allNodes[selectedEntry]
-								->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[2][2] = scaling[2];
-
-
-
-							currentScene.allNodes[selectedEntry]->updateTransformation();
-
-							glm::mat4 transformation = currentScene.allNodes[selectedEntry]
-								->getGlobalTransformation(selectedFrame, currentScene.animationIndex);
-
-							ImGui::Text("Transformation Matrix");
-							if (ImGui::BeginTable("Transformations", 4,
-								ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable
-								| ImGuiSelectableFlags_SpanAllColumns))
-							{
-								ImGui::TableNextRow();
-								for (u32 i = 0; i < transformation.length(); i++)
-								{
-									ImGui::TableNextRow();
-									for (u32 j = 0; j < transformation[i].length(); j++)
-									{
-										ImGui::TableNextColumn();
-										ImGui::Text("%.6f", transformation[i][j]);
-									}
-								}
-								ImGui::EndTable();
-							}
-						}
-					}
-
-
-				}
-			}
 			ImGui::TreePop();
 		}
 
@@ -213,19 +121,120 @@ void NoxEngineGUI::updateAnimationPanel(NoxEngine::GameState* game_state) {
 		sceneID++;
 		startItr++;
 	}
-
-
-	// If there was a mesh loaded by user
-	// TODO: Might be a good idea to move this to ScenePanel?
-	//       Have a queue of loaded meshes then add/update in ScenePanel if it's not empty
-	// if (isAlreadyLoaded) {
-	// 	mesh->prepForRenderer();
-	// 	game_state->renderer->addObject(mesh);
-	// 	game_state->renderer->updateBuffers();
-	// 	delete mesh;
-	// }
-
-
 	// Window End
 	ImGui::End();
+}
+
+void NoxEngineGUI::updateSequencer(NoxEngine::MeshScene& currentScene)
+{
+	if (currentScene.hasAnimations())
+	{
+		// Let try putting the sequencer here
+		if (ImGui::CollapsingHeader("Sequencer"))
+		{
+			AnimationSequencer mySequence(&currentScene);
+			mySequence.mFrameMin = 0;
+			mySequence.mFrameMax = currentScene.numTicks[currentScene.animationIndex] - 1;
+
+			// Let's create the sequencer
+			static int selectedEntry = -1;
+			static int firstFrame = 0;
+			static bool expanded = true;
+			int selectedFrame = currentScene.frameIndex;
+
+			ImGui::PushItemWidth(130);
+			ImGui::InputInt("Frame Min", &mySequence.mFrameMin);
+			ImGui::SameLine();
+			ImGui::InputInt("Frame ", &selectedFrame);
+			ImGui::SameLine();
+			ImGui::InputInt("Frame Max", &mySequence.mFrameMax);
+			currentScene.updateNumTicks(currentScene.animationIndex, mySequence.mFrameMax + 1);
+			ImGui::PopItemWidth();
+
+			// Safe guard
+			if (selectedFrame < 0)
+				selectedFrame = 0;
+			if (selectedFrame > currentScene.numTicks[currentScene.animationIndex] - 1)
+				selectedFrame = currentScene.numTicks[currentScene.animationIndex] - 1;
+
+			Sequencer(&mySequence, &selectedFrame, &expanded, &selectedEntry,
+				&firstFrame, ImSequencer::SEQUENCER_EDIT_STARTEND
+				| ImSequencer::SEQUENCER_CHANGE_FRAME);
+
+			currentScene.frameIndex = selectedFrame;
+
+			//add a UI to edit that particular item
+			if (selectedEntry != -1)
+			{
+				if (currentScene.allNodes[selectedEntry]->hasAnimations())
+				{
+					ImGui::Text("Translation");
+					glm::mat4 translationMatrix = currentScene.allNodes[selectedEntry]
+						->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame];
+					float translate[3] = { translationMatrix[0][3], translationMatrix[1][3], translationMatrix[2][3] };
+					ImGui::DragFloat3("Translation", translate, 0.005f, -100, 100, "%.3f", 0);
+					//Apply value
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[0][3] = translate[0];
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[1][3] = translate[1];
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimTranslationMatrices[currentScene.animationIndex][selectedFrame])[2][3] = translate[2];
+
+
+					ImGui::Text("Rotation");
+					float rotation[3] = { currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][0],
+						currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][1] ,
+						currentScene.allNodes[selectedEntry]->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][2] };
+					ImGui::DragFloat3("Rotation", rotation, 0.005f, -10, 10, "%.3f", 0);
+					//Apply value
+					currentScene.allNodes[selectedEntry]
+						->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][0] = rotation[0];
+					currentScene.allNodes[selectedEntry]
+						->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][1] = rotation[1];
+					currentScene.allNodes[selectedEntry]
+						->eulerAngleXYZ[currentScene.animationIndex][selectedFrame][2] = rotation[2];
+
+
+					ImGui::Text("Scaling");
+					glm::mat4 scalingMatrix = currentScene.allNodes[selectedEntry]
+						->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame];
+					float scaling[3] = { scalingMatrix[0][0], scalingMatrix[1][1], scalingMatrix[2][2] };
+					ImGui::DragFloat3("Scaling", scaling, 0.005f, 1, 100, "%.3f", 0);
+					//Apply value
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[0][0] = scaling[0];
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[1][1] = scaling[1];
+					(currentScene.allNodes[selectedEntry]
+						->nodeAnimScalingMatrices[currentScene.animationIndex][selectedFrame])[2][2] = scaling[2];
+
+
+
+					currentScene.allNodes[selectedEntry]->updateTransformation();
+
+					glm::mat4 transformation = currentScene.allNodes[selectedEntry]
+						->getGlobalTransformation(selectedFrame, currentScene.animationIndex);
+
+					ImGui::Text("Transformation Matrix");
+					if (ImGui::BeginTable("Transformations", 4,
+						ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable
+						| ImGuiSelectableFlags_SpanAllColumns))
+					{
+						ImGui::TableNextRow();
+						for (u32 i = 0; i < transformation.length(); i++)
+						{
+							ImGui::TableNextRow();
+							for (u32 j = 0; j < transformation[i].length(); j++)
+							{
+								ImGui::TableNextColumn();
+								ImGui::Text("%.6f", transformation[i][j]);
+							}
+						}
+						ImGui::EndTable();
+					}
+				}
+			}
+		}
+	}
 }
